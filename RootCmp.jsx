82f980da1ad62b8690ab0ad11e811e14c6d0d64e
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-19 17:25:59 +0300

    F/conflict-resolved

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,7 +2,7 @@ const { Route, Routes } = ReactRouterDOM;
 const Router = ReactRouterDOM.HashRouter;
 
 import { AppHeader } from './cmps/AppHeader.jsx';
-import { contactDetails } from './views/ContactDetails.jsx'; 
+import { ContactDetails } from './views/ContactDetails.jsx';
 import { Home } from './views/Home.jsx';
 import { MailIndex } from './apps/mail/views/MailIndex.jsx';
 import { NoteIndex } from './apps/note/views/NoteIndex.jsx';
