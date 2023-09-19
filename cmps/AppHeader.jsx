9a9e6d585cfe9f7a653a4c837012e02dcdb94203
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-19 21:30:20 +0300

    F/chang the app header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,10 +1,12 @@
 const { NavLink, Link, useNavigate, useParams } = ReactRouterDOM;
 export function AppHeader() {
-  <header className="app-header">
-    <span>dsadfdsfsd</span>
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/contact">Contacts</NavLink>
-    </nav>
-  </header>;
+  return (
+    <header className="app-header">
+      <span>dsadfdsfsd</span>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/contact">Contacts</NavLink>
+      </nav>
+    </header>
+  );
 }
