b3267aa2ab1376f48e26f619d78f0c4cf003caf0
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-19 21:07:24 +0300

    Merge branch 'main' into F/add-route-ContactDetails

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,3 +1,25 @@
+import { AppHeader } from './cmps/AppHeader.jsx';
+import { contactDetails } from './views/ContactDetails.jsx';
+import { Home } from './views/Home.jsx';
+import { MailIndex } from './apps/mail/views/MailIndex.jsx';
+import { NoteIndex } from './apps/note/views/NoteIndex.jsx';
+import { ContantIndex } from './cmps/ContactIndex.jsx';
+
+export function App() {
+  return (
+    <Router>
+      <section className="app">
+        {/* <AppHeader /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact:contactId" element={<ContactDetails />} />
+          <Route path="/edit:contactId" element={<ContactEdit />} />
+          <Route path="/contact" element={<ContantIndex />} />
+        </Routes>
+      </section>
+    </Router>
+  );
+}
 const { Route, Routes } = ReactRouterDOM;
 const Router = ReactRouterDOM.HashRouter;
 const { Provider } = ReactRedux;
@@ -16,7 +38,7 @@ export function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact/:contactId" element={<ContactDetails />} />
-            <Route path="/edit/:contactId" element={<ContactEdit />} />
+
             <Route path="/contact" element={<ContantIndex />} />
           </Routes>
         </section>
