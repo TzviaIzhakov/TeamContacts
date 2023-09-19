324885e22aa3054774cd1a6be9d10fb0faff839c
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-19 16:47:30 +0300

    F/add-route-contact-index

--- cmps/ContactIndex.jsx
+++ cmps/ContactIndex.jsx
@@ -13,30 +13,29 @@ export function ContantIndex() {
       showErrorMsg('Cannot load cars');
     });
   }, []);
-}
 
-function onUpdateContact(contact) {
-  const contactToSave = contact;
-  saveContact(contactToSave)
-    .then((savedContact) => {
-      showSuccessMsg(`Contact added (id: ${savedContact._id})`);
-    })
-    .catch((err) => {
-      console.log('Cannot update contact', err);
-      showErrorMsg('Cannot update contact');
-    });
-}
+  function onUpdateContact(contact) {
+    const contactToSave = contact;
+    saveContact(contactToSave)
+      .then((savedContact) => {
+        showSuccessMsg(`Contact added (id: ${savedContact._id})`);
+      })
+      .catch((err) => {
+        console.log('Cannot update contact', err);
+        showErrorMsg('Cannot update contact');
+      });
+  }
 
-function onRemoveContact(contactId) {
-  removeContactOptimistic(contactId)
-    .then(() => {
-      showSuccessMsg('Contact removed');
-    })
-    .catch((err) => {
-      console.log('Cannot remove contact', err);
-      showErrorMsg('Cannot remove contact');
-    });
-
+  function onRemoveContact(contactId) {
+    removeContactOptimistic(contactId)
+      .then(() => {
+        showSuccessMsg('Contact removed');
+      })
+      .catch((err) => {
+        console.log('Cannot remove contact', err);
+        showErrorMsg('Cannot remove contact');
+      });
+  }
   if (!conts) return <div>loading...</div>;
   return (
     <section>
