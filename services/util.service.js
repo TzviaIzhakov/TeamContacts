68b8cebba88f17868c1d59e0827675432df13baa
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-19 15:34:51 +0300

    F/add-contact-service

--- services/util.service.js
+++ services/util.service.js
@@ -2,10 +2,8 @@ export const utilService = {
     makeId,
     makeLorem,
     getRandomIntInclusive,
-    getRandomColor,
-    padNum,
-    getDayName,
-    getMonthName,
+    loadFromStorage,
+    saveToStorage
 }
 
 function makeId(length = 6) {
@@ -35,28 +33,11 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
-function padNum(num) {
-    return (num > 9) ? num + '' : '0' + num
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
 }
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF'
-    var color = '#'
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)]
-    }
-    return color
-}
-
-function getDayName(date, locale) {
-    date = new Date(date)
-    return date.toLocaleDateString(locale, { weekday: 'long' })
-}
-
-
-function getMonthName(date) {
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ]
-    return monthNames[date.getMonth()]
+function loadFromStorage(key) {
+    const data = localStorage.getItem(key)
+    return (data) ? JSON.parse(data) : undefined
 }
